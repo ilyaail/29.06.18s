e31781f2043108e736ec013d204a23f73cdbfa08
ilyaail <40268706+ilyaail@users.noreply.github.com>
2018-07-09 00:08:42 +0300

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load((getClass().getResource("sample.fxml")));
+        primaryStage.setTitle("Склад");
+        primaryStage.setScene(new Scene(root, 905, 605));
+
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
